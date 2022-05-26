eae9433baf0004f2e08c120db9fe265655ff7976
Pedro Martos <54295971+P2D3R4@users.noreply.github.com>
2022-05-26 23:16:02 +0000

    sdfsdfs

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask('__name__')
-
-
-@app.route('/')
+@app.route('/Meu primeiro template')
 def index():
     return render_template('/index.html')
-
 
 @app.route('/unifran')
 def unifran():
     return render_template('/unifran.html')
 
-
-if __name__ == '__name__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
